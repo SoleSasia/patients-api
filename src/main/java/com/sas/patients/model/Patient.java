d45f0155c85f71eb6a1ada85540dd8bc1e1c8987
Soledad <soledad.sasia@gmail.com>
2026-08-13 11:33:12 -0300

    Implement Patient model

--- src/main/java/com/sas/patients/model/Patient.java
+++ src/main/java/com/sas/patients/model/Patient.java
@@ -1,4 +1,28 @@
 package com.sas.patients.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String dni;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String telephone;
+    private String healthCare;
 }
