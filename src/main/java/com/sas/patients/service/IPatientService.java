e03bc2e2054de44d32e40155ccce7b1c2b429341
Soledad <soledad.sasia@gmail.com>
2026-08-13 11:36:55 -0300

    Implement IPatientService interface

--- src/main/java/com/sas/patients/service/IPatientService.java
+++ src/main/java/com/sas/patients/service/IPatientService.java
@@ -1,4 +1,15 @@
 package com.sas.patients.service;
 
+import com.sas.patients.model.Patient;
+
+import java.util.List;
+
 public interface IPatientService {
+
+    List<Patient> findAll();
+    List<Patient> findByName(String name);
+    Patient findByDni(String dni);
+    Patient createPatient(Patient patient);
+    Patient updatePatient(Long id, Patient patient);
+    void deletePatient(Long id);
 }
