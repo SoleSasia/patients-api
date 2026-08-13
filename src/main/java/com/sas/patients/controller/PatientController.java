1b4433197910aaacd337dd26e1b5d09b5c90739c
Soledad <soledad.sasia@gmail.com>
2026-08-13 13:00:07 -0300

    Implement PatientController

--- src/main/java/com/sas/patients/controller/PatientController.java
+++ src/main/java/com/sas/patients/controller/PatientController.java
@@ -1,15 +1,38 @@
 package com.sas.patients.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sas.patients.model.Patient;
+import com.sas.patients.service.PatientService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class PatientController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello Patient Controller";
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping("/patients")
+    public List<Patient> getPatients() {
+        return patientService.getPatients();
+    }
+    
+    @GetMapping("/patient/{name}")
+    public List<Patient> getPatientsByName(@PathVariable String name) {
+        return patientService.getPatientsByName(name);
+    }
+
+    @GetMapping("/patient/{dni}")
+    public Patient getPatientByDni(@PathVariable String dni) {
+        return patientService.getPatientByDni(dni);
+    }
+
+    @PostMapping
+    public Patient createPatient(@RequestBody Patient patient) {
+        return patientService.createPatient(patient);
     }
 }
